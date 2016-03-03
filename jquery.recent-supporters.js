fd5bd9795d7b0323e50faab91edac8ca99a4ac94
Alex Berger <alex@more-onion.com>
2016-03-03 12:56:56 +0100

    use an UMD header

--- jquery.recent-supporters.js
+++ jquery.recent-supporters.js
@@ -7,7 +7,26 @@
  * https://github.com/moreonion/jquery-recent-supporters/blob/master/LICENSE
  */
 
-(function ( $ ) {
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['jquery'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = function( root, jQuery ) {
+      if ( jQuery === undefined ) {
+        if ( typeof window !== 'undefined' ) {
+          jQuery = require('jquery');
+        }
+        else {
+          jQuery = require('jquery')(root);
+        }
+      }
+      factory(jQuery);
+      return jQuery;
+    };
+  } else {
+    factory(jQuery);
+  }
+}(function ($) {
 
   $.fn.recentSupporters = function( options ) {
 
@@ -447,5 +466,4 @@
 
     return this;
   }
-})(jQuery);
-
+}));
